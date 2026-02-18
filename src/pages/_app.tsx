9cb6074728712055f7f8836891045fa49c9add0e
softgenai <admin@softgen.ai>
2026-02-18 15:08:15 +0000

    fix(config): Remove problematic script and simplify Next.js setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/pos.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@/contexts/ThemeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
